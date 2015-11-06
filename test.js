8e46631fb611cd13c45fe3113a27a75f85a2282a
Andrey Sitnik <andrey@sitnik.ru>
2015-11-06 13:05:37 +0300

    Add Russian-English convertation support

--- test.js
+++ test.js
@@ -9,4 +9,24 @@ describe('convert-layout', function () {
         expect(main.ru).to.eql(ru);
     });
 
+    describe('ru', function () {
+
+        it('converts lowers to English', function () {
+            expect(ru.fromEn('ntcn/')).to.eql('тест.');
+        });
+
+        it('converts uppers to English', function () {
+            expect(ru.fromEn('NTCN?&')).to.eql('ТЕСТ,?');
+        });
+
+        it('converts lowers from English', function () {
+            expect(ru.toEn('еуыею')).to.eql('test.');
+        });
+
+        it('converts uppers from English', function () {
+            expect(ru.toEn('ЕУЫЕ№')).to.eql('TEST#');
+        });
+
+    });
+
 });
